bef736cfbfa6a78d98f1d531447843caa1f16819
Squaredcandy <squaredcandy@gmail.com>
2023-05-30 00:14:01 +1000

    Making Identifier extend Serializable

--- waypoint-core/src/main/java/com/squaredcandy/waypoint/core/Identifier.kt
+++ waypoint-core/src/main/java/com/squaredcandy/waypoint/core/Identifier.kt
@@ -1,9 +1,10 @@
 package com.squaredcandy.waypoint.core
 
+import java.io.Serializable
 import java.util.UUID
 
 @Suppress("unused")
 @JvmInline
-value class Identifier<T>(val id: String)
+value class Identifier<T>(val id: String): Serializable by id
 
 fun <T> randomIdentifier() = Identifier<T>(UUID.randomUUID().toString())

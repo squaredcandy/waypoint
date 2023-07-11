1d0b6d29defe26f281bf654356728898161f980c
Squaredcandy <squaredcandy@gmail.com>
2023-07-12 00:50:09 +1000

    Remove Waypoint Content Modifier
    Signed-off-by: Squaredcandy <squaredcandy@gmail.com>

--- waypoint-core/src/main/java/com/squaredcandy/waypoint/core/content/EmptyWaypointContentScope.kt
+++
@@ -1,8 +0,0 @@
-package com.squaredcandy.waypoint.core.content
-
-import androidx.compose.ui.modifier.ModifierLocal
-
-internal object EmptyWaypointContentScope : WaypointContentScope {
-    override val <T> ModifierLocal<T>.currentOrNull: T?
-        get() = null
-}
--- waypoint-core/src/main/java/com/squaredcandy/waypoint/core/content/WaypointContentScope.kt
+++
@@ -1,7 +0,0 @@
-package com.squaredcandy.waypoint.core.content
-
-import androidx.compose.ui.modifier.ModifierLocal
-
-sealed interface WaypointContentScope {
-    val <T> ModifierLocal<T>.currentOrNull: T?
-}
